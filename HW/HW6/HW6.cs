5799246aff96a9f3cf1f7e4bf123f9bc5b6c9470
Fraiters <67577747+Fraiters@users.noreply.github.com>
2021-02-26 16:32:43 +0300

    Add files via upload

---
+++ HW/HW6/HW6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class HW6
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string zodiac;
+            string work;
+
+            Console.Write("Как вас зовут ? ");
+            name = Console.ReadLine();
+
+            Console.Write("Сколько вам лет? ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Какой ваш знак зодиака? ");
+            zodiac = Console.ReadLine();
+
+            Console.Write("Какая у вас профессия? ");
+            work = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут " + name + ", вам " + age + " лет" + ", вы " + zodiac + " и ваша профессия - " + work);
+        }
+    }
+}
