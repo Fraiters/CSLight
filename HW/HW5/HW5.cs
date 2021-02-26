b5f0393d4a12b6962823b708c5510e4dfab44e07
Fraiters <67577747+Fraiters@users.noreply.github.com>
2021-02-26 16:15:20 +0300

    Add files via upload

---
+++ HW/HW5/HW5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class HW5
+    {
+        static void Main(string[] args)
+        {
+            int countPeople;
+            int timePerson = 10;
+            int timeWaiting;
+            int hour, minute;
+
+            Console.Write("Введите количество старушек в очереди - ");
+            countPeople = Convert.ToInt32(Console.ReadLine());
+
+            timeWaiting = countPeople * timePerson;
+            hour = timeWaiting / 60;
+            minute = timeWaiting % 60;
+
+            Console.WriteLine("Вам осталось ждать " + hour + " часа " + minute + " минут");
+        }
+    }
+}
