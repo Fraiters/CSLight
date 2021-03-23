7b62d571ad9951869a8be3b228af8e896db8b4e4
Fraiters <67577747+Fraiters@users.noreply.github.com>
2021-03-23 13:59:42 +0300

    Add files via upload

---
+++ HW/Основы программирования/HW1/HW1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class HW1
+    {
+        static void Main(string[] args)
+        {
+            int time;
+            bool isOpen;
+            char letter;
+            string name;
+            float speed;
+            double impulse;
+            short number;
+            byte height;
+            uint weight;
+            ushort length;
+        }
+    }
+}
+
---
+++ HW/Основы программирования/HW5/HW5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class HW5
+    {
+        static void Main(string[] args)
+        {
+            int countPeople;
+            int timePerson = 10;
+            int timeWaiting;
+            int hourWaiting;
+            int minuteWaiting;
+
+            Console.Write("Введите количество старушек в очереди - ");
+            countPeople = Convert.ToInt32(Console.ReadLine());
+
+            timeWaiting = countPeople * timePerson;
+            hourWaiting = timeWaiting / 60;
+            minuteWaiting = timeWaiting % 60;
+
+            Console.WriteLine("Вам осталось ждать " + hourWaiting + " часа " + minuteWaiting + " минут");
+        }
+    }
+}
