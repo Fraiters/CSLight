019650c7eb98f1facfdd4cec2fd8f2c0545a5c5a
Fraiters <67577747+Fraiters@users.noreply.github.com>
2021-03-23 14:04:49 +0300

    Add files via upload

---
+++ HW/Условные операторы и циклы/HW1/HW1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class HW1
+    {
+        static void Main(string[] args)
+        {
+            string message = "Да прибудет с тобой сила!";
+            int n;
+
+            Console.Write("Введите количество повторений: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(message);
+            }
+            
+        }
+    }
+}
