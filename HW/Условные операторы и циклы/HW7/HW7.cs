ccbb5f6ed17c4b8126430f49a2fd5c30d9a9ead0
Fraiters <67577747+Fraiters@users.noreply.github.com>
2021-06-28 13:23:42 +0300

    Add files via upload

---
+++ HW/Условные операторы и циклы/HW7/HW7.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class HW7
+    {
+        static void Main(string[] args)
+        {
+            string truePassword = "0000";
+            string password;
+            int numberAttempt = 3;
+            string secretMessage = "Не ссы против ветра (Дж.Стэтхэм)";
+
+            while (numberAttempt > 0)
+            {
+                Console.Write("Чтобы получить секретное сообщение введите пароль: ");
+                password = Console.ReadLine();
+
+                if (truePassword == password)
+                {
+                    Console.WriteLine(secretMessage);
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Пароль неверный \n");
+                    numberAttempt--;
+                }
+            }
+
+            if (numberAttempt <= 0)
+                Console.WriteLine("У вас не осталось попыток ввести пароль, программа завершается");
+
+            Console.ReadKey();
+        }
+    }
+}
