cbb149f0e607079996bcf2d7a099c0f1f381a2ea
Fraiters <67577747+Fraiters@users.noreply.github.com>
2021-06-28 11:58:28 +0300

    Add files via upload

---
+++ HW/Условные операторы и циклы/HW5/HW5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class HW5
+    {
+        static void Main(string[] args)
+        {
+            int step = 7;
+            int maxNumber = 98;
+            int memberSequence = 0;
+
+            while (memberSequence != maxNumber)
+            {
+                memberSequence += step;
+                Console.Write(" " + memberSequence);
+            }
+            Console.ReadKey();
+        }
+    }
+}
