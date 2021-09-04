44c81fd016442b64b68c9eb486a068fd0e666c84
Fraiters <67577747+Fraiters@users.noreply.github.com>
2021-09-05 01:49:30 +0300

    Add files via upload

--- HW/Массивы/HW1/HW1.cs
+++ HW/Массивы/HW1/HW1.cs
@@ -10,29 +10,28 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            int[,] array = { 
-                {1, 4, 7},
-                {10, 7, 4},
-                {95, 4, 6} };
+            int[,] array = { {1, 4, 7}, {10, 7, 4}, {95, 4, 6} };
 
-            int i;
-            int j ;
             int sumString2 = 0;
             int productColumn1 = 1;
 
             Console.WriteLine("Исходная матрица:");
 
-            for (i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                sumString2 = 0;
-                for(j = 0; j < array.GetLength(1); j++)
+                for(int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write(array[i, j] + " ");
-                    sumString2 += array[1, j];
                 }             
                 productColumn1 *= array[i, 0];
                 Console.WriteLine();
             }
+
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                sumString2 += array[1, j];
+            }
+
             Console.WriteLine("Сумма 2ой строки: " + sumString2);
             Console.WriteLine("Произведение 1го столбца: " + productColumn1);
 
